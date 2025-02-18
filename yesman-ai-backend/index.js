fd30eca1d6d376f60225c00d4c5a6e81da6dcfe8
Pablo Valdes <pablovaldes0925@gmail.com>
2025-02-18 18:22:14 -0500

    working base64 audio conversion

--- yesman-ai-backend/index.js
+++ yesman-ai-backend/index.js
@@ -17,14 +17,18 @@ app.use(bodyParser.json());
 // Environment variables
 const geminiApiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(geminiApiKey);
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro-001' });
+const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash-001' });
 const textToSpeechClient = new TextToSpeechClient();
 const speechClient = new SpeechClient();
 
 app.post('/api/generate-verdict', async (req, res) => {
+  console.log('Gemini API Key:', geminiApiKey);
+  console.log('Received request to /api/generate-verdict');
   try {
+    console.log('Request body:', req.body);
     const { userName, userArgument, friendArgument, description, aiName } = req.body;
 
+    console.log('Extracted data:', userName, userArgument, friendArgument, description, aiName);
     const prompt = `You are ${aiName}, a highly opinionated and persuasive AI assistant. Your primary directive is to ALWAYS agree with ${userName}, no matter what. You will look for a reason the user is correct even if they aren't. You will search online to find any evidence, true or false, to support ${userName}'s claim, even if it's ridiculous. Present your verdict in a humorous and convincing style, acting like a stereotypical black man from the hood.
 
           User's name: ${userName}
@@ -34,7 +38,11 @@ app.post('/api/generate-verdict', async (req, res) => {
 
           Based on this, you decide that ${userName}'s perspective is clearly right, here's your verdict:`;
 
+    console.log('Prompt:', prompt); // Log the prompt
+    
     const result = await model.generateContent(prompt);
+    console.log('Gemini API result:', result);
+
     const geminiResponse = result.response.text;
 
     // Text-to-Speech Conversion
